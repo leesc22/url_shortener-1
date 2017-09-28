9ea2c0c339bdbf290953230f18bf905ebd1b2858
leesc22 <leesiewching22@gmail.com>
2017-09-28 14:36:31 +0800

    Pass url model unit test

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,3 +1,9 @@
 class Url < ApplicationRecord
+	validates :long_url, :short_url, presence: true
+	validates :long_url, format: { with: URI.regexp }, allow_blank: true
 
+	def self.retrieve_short_url(long_url)
+		url = self.find_by(long_url: long_url)
+		url.short_url if url
+	end
 end
